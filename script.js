eb8c42f69dd7f5f6a309efde07969f77ecc7cfb8
Dina Dahliana <dina.dahliana@students.itb.ac.id>
2022-11-18 11:00:49 +0700

    Create function to play one round of the game

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-// Create function to get random computer choice
+// function to get random computer choice
 function getComputerChoice(){
     const choice = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * choice.length);
     const computerChoice = choice[randomIndex];
 
-    console.log(computerChoice)
+    return computerChoice;
 }
 
-getComputerChoice()
+// function to play one round of the game
+function playRound(playerSelection, computerSelection) {
+    if 
+        (computerSelection === "Rock" && playerSelection === "paper" ||
+        computerSelection === "Paper" && playerSelection === "scissors" ||
+        computerSelection === "Scissors" && playerSelection === "rock") {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        }
+    else if 
+        (computerSelection === "Rock" && playerSelection === "scissors" ||
+        computerSelection === "Paper" && playerSelection === "rock" ||
+        computerSelection === "Scissors" && playerSelection === "paper") {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        }
+    else {
+        console.log("Draw")
+    }
+}
+
+let playerSelection = "paper";
+let computerSelection = "Scissors";
+playRound(playerSelection, computerSelection)
