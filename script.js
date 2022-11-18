8523c1eb1eae8d7cc2a8b2c6bc7941444672d9c1
Dina Dahliana <dina.dahliana@students.itb.ac.id>
2022-11-18 11:35:46 +0700

    Create function to play a 5 round game

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// initialize score 
+let playerScore = 0;
+let computerScore = 0;
+
 // function to get random computer choice
 function getComputerChoice(){
     const choice = ["Rock", "Paper", "Scissors"];
@@ -13,19 +17,40 @@ function playRound(playerSelection, computerSelection) {
         (computerSelection === "Rock" && playerSelection === "paper" ||
         computerSelection === "Paper" && playerSelection === "scissors" ||
         computerSelection === "Scissors" && playerSelection === "rock") {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            console.log(`You win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`);
+            playerScore += 1;
         }
     else if 
         (computerSelection === "Rock" && playerSelection === "scissors" ||
         computerSelection === "Paper" && playerSelection === "rock" ||
         computerSelection === "Scissors" && playerSelection === "paper") {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            console.log(`You lose! ${computerSelection} beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1)}`);
+            computerScore +=1;
         }
     else {
         console.log("Draw")
     }
 }
 
-let playerSelection = "paper";
-let computerSelection = "Scissors";
-playRound(playerSelection, computerSelection)
+// function to play 5 round game
+function game() {
+    for (let i=1; i<=5; i++){
+        const playerSelection = prompt("Choose your weapon");
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection.toLowerCase(), computerSelection);
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+
+    console.log(`Finish result
+                Player score: ${playerScore}
+                Computer score: ${computerScore}`);
+
+    if (playerScore > computerScore) {
+        console.log(`Congratulations! You win the game!`)
+    } else {
+        console.log(`Nice try!`)
+    }
+}
+
+game()
